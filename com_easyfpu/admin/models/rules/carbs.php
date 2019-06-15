4ac45603d16a8efde7a5389743baecb7fd0b9e00
UlricusR <ulrich.rueth@gmail.com>
2019-06-15 17:19:00 +0200

    Introduced rule to check that cals from carbs do not exceed total cals

---
+++ com_easyfpu/admin/models/rules/carbs.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_easyfpu
+ *
+ * @copyright   Copyright (C) 2019 Ulrich Rueth. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+// Imports
+use Joomla\CMS\Form\FormRule;
+use Joomla\Registry\Registry;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Language\Text;
+
+/**
+ * Form Rule class for the Joomla Framework.
+ */
+class JFormRuleCarbs extends FormRule
+{
+    public function test(SimpleXMLElement $element, $value, $group = null, Registry $input = null, Form $form = null)
+    {
+        // Check if value is numeric
+        if (!is_numeric($value)) {
+            $element->attributes()->message = Text::_('COM_EASYFPU_EASYFPU_ERRMSG_NUMBER_FRAC');
+            return false;
+        }
+        
+        var_dump($form);
+
+        // Check if calories from carbs do not exceed total calories (1g carbs has 4 kcal)
+        $caloriesFromCarbs = $value * 4;
+        $totalCalories = $form->getValue('calories');
+        
+        if ($caloriesFromCarbs > $totalCalories) {
+            $element->attributes()->message = Text::_('COM_EASYFPU_EASYFPU_ERRMSG_TOOMUCHCARBS');
+            return false;
+        }
+        
+        return true;
+    }
+}
