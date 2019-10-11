9340f4c7d9d31aabb29c4a85fb6b6021fd8b4e9d
UlricusR <ulrich.rueth@gmail.com>
2019-10-11 22:27:15 +0200

    Refactored

--- com_easyfpu/admin/services/provider.php
+++ com_easyfpu/admin/services/provider.php
@@ -11,13 +11,10 @@
 defined('_JEXEC') or die;
 
 // Imports
-use Joomla\CMS\Component\Router\RouterFactoryInterface;
 use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
-use Joomla\CMS\Extension\Service\Provider\CategoryFactory;
 use Joomla\CMS\Extension\Service\Provider\ComponentDispatcherFactory;
 use Joomla\CMS\Extension\Service\Provider\MVCFactory;
-use Joomla\CMS\Extension\Service\Provider\RouterFactory;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
@@ -42,10 +39,8 @@ return new class implements ServiceProviderInterface
      */
     public function register(Container $container)
     {
-        $container->registerServiceProvider(new CategoryFactory('\\RuethInfo\\Component\\Easyfpu'));
         $container->registerServiceProvider(new MVCFactory('\\RuethInfo\\Component\\Easyfpu'));
         $container->registerServiceProvider(new ComponentDispatcherFactory('\\RuethInfo\\Component\\Easyfpu'));
-        $container->registerServiceProvider(new RouterFactory('\\RuethInfo\\Component\\Easyfpu'));
         $container->set(
             ComponentInterface::class,
             function (Container $container)
@@ -54,8 +49,6 @@ return new class implements ServiceProviderInterface
                 
                 $component->setRegistry($container->get(Registry::class));
                 $component->setMVCFactory($container->get(MVCFactoryInterface::class));
-                // $component->setCategoryFactory($container->get(CategoryFactoryInterface::class));
-                $component->setRouterFactory($container->get(RouterFactoryInterface::class));
                 
                 return $component;
             }
